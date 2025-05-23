046578f22e815822101bd7e00db774a45acbfaf2
allenwuzhiwei <allenzhiwei0@gmail.com>
2025-05-23 17:43:41 +0800

    add k8s

--- src/main/java/com/nusiss/apigateway/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/nusiss/apigateway/filter/TokenAuthenticationFilter.java
@@ -3,41 +3,51 @@ package com.nusiss.apigateway.filter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nusiss.apigateway.exception.CustomException;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.core.ParameterizedTypeReference;
+import com.nusiss.apigateway.util.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.*;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.filter.OncePerRequestFilter;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 
 @Component
-public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        ServerHttpResponse response = exchange.getResponse();
-        /*String uri = request.getPath().toString();
-        String method = exchange.getRequest().getMethod().toString();
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
         //all requests need to validate except for /validateUserAndPassword and /login
         if(!uri.contains("/validateUserAndPassword")
                 && !uri.contains("/login")
                 && !uri.contains("/validateToken")
                 && !uri.contains("swagger")){
-            String token = request.getHeaders().getFirst("authToken");
-
-            try {
+            String token = null;
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+            }
+            if(jwtUtils.isValid(token)){
+                throw new CustomException("Invalid token.");
+            }
+            /*try {
                 if(!validateToken(token)){
                     throw new CustomException("Invalid token.");
                 }
@@ -58,14 +68,15 @@ public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
                 }
             } catch (Exception e) {
                 return handleException(response, e);
-            }
-        }*/
+            }*/
+        }
 
         // If token is valid, proceed to the next filter in the chain
-        return chain.filter(exchange);
+        filterChain.doFilter(request, response);
     }
 
-    private boolean permission(String token, String url, String method) throws Exception {
+
+    /*private boolean permission(String token, String url, String method) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
         StringBuffer fullurl = new StringBuffer("http://localhost:8084/permission?url=");
         fullurl.append(url).append("&method=").append(method);
@@ -76,8 +87,8 @@ public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
         headers.add("authToken", token);
 
         // Create the request body
-        /*Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("authToken", token);*/
+        *//*Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("authToken", token);*//*
 
         // Create the HttpEntity object containing headers and the body
         HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(headers);
@@ -92,7 +103,7 @@ public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
         Boolean isValidated = response.getBody();
 
         return isValidated;
-    }
+    }*/
 
     private Mono<Void>  handleException(ServerHttpResponse response, Exception ex) {
         // Prepare response structure
@@ -118,9 +129,9 @@ public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
 
     }
 
-    private boolean validateToken(String token) throws Exception {
+    /*private boolean validateToken(String token) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://nusmall.com:8084/validateToken";
+        String url = "http://localhost:8084/validateToken";
 
         // Create headers
         HttpHeaders headers = new HttpHeaders();
@@ -143,11 +154,7 @@ public class TokenAuthenticationFilter implements GlobalFilter, Ordered {
         Boolean isValidated = response.getBody();
 
         return isValidated;
-    }
+    }*/
 
 
-    @Override
-    public int getOrder() {
-        return -1;  // Set filter order; lower value means higher precedence
-    }
 }
